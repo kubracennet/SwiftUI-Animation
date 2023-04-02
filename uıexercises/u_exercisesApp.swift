21d100e1d19b1fbb9bb44b78c88e4352699cd105
kubracennet <kubracennetyavasoglu@gmail.com>
2023-04-02 22:34:23 +0300

    Initial Commit

---
+++ uıexercises/u_exercisesApp.swift
@@ -0,0 +1,17 @@
+//
+//  u_exercisesApp.swift
+//  uıexercises
+//
+//  Created by Kübra Cennet Yavaşoğlu on 2.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct u_exercisesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
